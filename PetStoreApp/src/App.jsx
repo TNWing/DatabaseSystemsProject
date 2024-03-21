71f4e7adde34a355ff020928fe63e54b86f1c381
Dante <dante.amicarella@gmail.com>
2024-03-21 18:04:57 -0400

    working carousel image

--- PetStoreApp/src/App.jsx
+++ PetStoreApp/src/App.jsx
@@ -7,41 +7,90 @@ function App() {
   return (
     <div>
       <header data-bs-theme="dark">
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">PetRescueNetwork</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">PetRescueNetwork</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-              <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">ADOPT</a>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">ADOPT</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">DONATE</a>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">DONATE</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">VOLUNTEER</a>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">VOLUNTEER</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">RESOURCES</a>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">RESOURCES</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active">SIGN IN</a>
+                <li className="nav-item">
+                  <a className="nav-link active">SIGN IN</a>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
       </header>
+      <main>
+        <div id="myCarousel" className="carousel slide mb-6" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+                <img src="src/images/petImage1.jpg" className="d-block w-100" alt="Slide 1"/>
+                <div className="container">
+                    <div className="carousel-caption text-start">
+                        <h1>Example headline.</h1>
+                        <p className="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
+                        <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                    </div>
+                </div>
+            </div>
+            <div className="carousel-item">
+              <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
+              <div className="container">
+                <div className="carousel-caption">
+                  <h1>Another example headline.</h1>
+                  <p>Some representative placeholder content for the second slide of the carousel.</p>
+                  <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
+              <div className="container">
+                <div className="carousel-caption text-end">
+                  <h1>One more for good measure.</h1>
+                  <p>Some representative placeholder content for the third slide of this carousel.</p>
+                  <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
 
 export default App;
 
-{/* <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+{/* <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button className="btn btn-outline-success" type="submit">Search</button>
               </form> */}
